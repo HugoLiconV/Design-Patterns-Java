680b846db30c48b3ed850f83b2cd5d0e7328b69b
Hugo <hugo_val28@hotmail.com>
2018-09-19 23:18:46 -0600

    add builder pattern

--- src/creational/builder/Builder.java
+++ src/creational/builder/Builder.java
@@ -2,4 +2,88 @@ package creational.builder;
 
 public class Builder {
 
+  public static void main(String[] args) {
+    BankAccount account = new BankAccount.Builder(1234L)
+        .withOwner("Marge")
+        .atBranch("Springfield")
+        .openingBalance(100)
+        .atRate(2.5)
+        .build();
+
+    Test prueba = new Test();
+    prueba.printHello().printWorld();
+  }
+}
+
+
+class Test {
+  Test printHello() {
+    System.out.print("Hello ");
+    return this; // TODO: 19/09/18 Que regresa this?
+  }
+
+  Test printWorld() {
+    System.out.println("World!");
+    return this;
+  }
+}
+
+class BankAccount {
+
+  private long accountNumber; //This is important, so we'll pass it to the constructor.
+  private String owner;
+  private String branch;
+  private double balance;
+  private double interestRate;
+
+  public static class Builder {
+
+    private long accountNumber; //This is important, so we'll pass it to the constructor.
+    private String owner;
+    private String branch;
+    private double balance;
+    private double interestRate;
+
+    public Builder(long accountNumber) {
+      this.accountNumber = accountNumber;
+    }
+
+    public Builder withOwner(String owner) {
+      this.owner = owner;
+      return this;  //By returning the builder each time, we can create a fluent interface.
+    }
+
+    public Builder atBranch(String branch) {
+      this.branch = branch;
+      return this;
+    }
+
+    public Builder openingBalance(double balance) {
+      this.balance = balance;
+      return this;
+    }
+
+    public Builder atRate(double interestRate) {
+      this.interestRate = interestRate;
+      return this;
+    }
+
+    public BankAccount build() {
+      //Here we create the actual bank account object, which is always in a fully initialised state when it's returned.
+      BankAccount account = new BankAccount();  //Since the builder is in the BankAccount class, we can invoke its private constructor.
+      account.accountNumber = this.accountNumber;
+      account.owner = this.owner;
+      account.branch = this.branch;
+      account.balance = this.balance;
+      account.interestRate = this.interestRate;
+      return account;
+    }
+  }
+
+  //Fields omitted for brevity.
+  private BankAccount() {
+    //Constructor is now private.
+  }
+  //Getters and setters omitted for brevity.
+
 }
--- src/creational/builder/builder.md
+++ src/creational/builder/builder.md
@@ -1,6 +1,9 @@
 #Builder
 ##Description
-
+Builder is a creational design pattern that lets you produce different types and representations of an object using the same building process. Builder allows constructing complex objects step by step
 ##Pros
-
-##Contras
+ * Allows building products step by step.
+ * Allows using the same code for building different products.
+ * Isolates the complex construction code from a product's core business logic.
+##Cons
+* Increases overall code complexity by creating multiple additional classes.
