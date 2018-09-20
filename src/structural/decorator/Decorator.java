bc9a2932dbb3a5b4601f7c353dbb2ab5ef0069fb
Hugo <hugo_val28@hotmail.com>
2018-09-20 11:40:02 -0600

    add decorator

---
+++ src/structural/decorator/Decorator.java
@@ -0,0 +1,68 @@
+package structural.decorator;
+
+public class Decorator {
+
+  public static void main(String[] args) {
+    System.out.println("Test 1\n");
+    Phone phone = new AndroidPhone(new BasicPhone());
+    phone.printModel();
+    System.out.println("\nTest 2\n");
+    Phone iPhone = new IPhone(phone);
+    iPhone.printModel();
+  }
+}
+
+interface Phone {
+  void printModel();
+}
+
+class BasicPhone implements Phone {
+
+  @Override
+  public void printModel() {
+    System.out.println("Basic Phone");
+  }
+}
+
+class PhoneDecorator implements Phone {
+
+  public Phone phone;
+
+  public PhoneDecorator(Phone phone) {
+    this.phone = phone;
+  }
+
+  @Override
+  public void printModel() {
+    this.phone.printModel();
+  }
+}
+
+class AndroidPhone extends PhoneDecorator {
+
+  public AndroidPhone(Phone phone) {
+    super(phone);
+  }
+
+  @Override
+  public void printModel() {
+    super.printModel();
+    System.out.println("Adding Features of Android");
+  }
+
+}
+
+class IPhone extends PhoneDecorator {
+
+  public IPhone(Phone phone) {
+    super(phone);
+  }
+
+  @Override
+  public void printModel() {
+    super.printModel();
+    System.out.println("Adding Features of iPhone");
+  }
+
+  // TODO: 20/09/18 how to Add new methods? 
+}
--- src/structural/decorator/decorator.md
+++ src/structural/decorator/decorator.md
@@ -1,6 +1,19 @@
-#Decorator
-##Description
+# Decorator
+## Description
+Decorator is a structural design pattern that lets you attach new behaviors to objects by placing them inside wrapper objects that contain these behaviors.
 
-##Pros
+You have to add and remove responsibilities from an object dynamically, but do it in a way so that it stay compatible with the rest of application's code.
 
-##Contras
+
+## Pros
+* Much more flexible than class inheritance.
+* Allows adding and removing behaviors at runtime.
+* Allows combining several additional behaviors by using multiple wrappers.
+* Allows composing complex objects from simple ones instead of having monolithic classes that implement every variant of behavior.
+
+## Contras
+* It is hard to configure a multi-wrapped object.
+* Lots of small classes.
+
+## Source
+* [Prototype - Refactoring Guru](https://refactoring.guru/design-patterns/prototype)
